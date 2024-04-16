517b9500444133b414dbb78a7b56de7847c44225
Zinedean Meaker Saaiman <ST10348753@vcconnect.edu.za>
2024-04-16 19:32:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ST10348753_PROG6221_POE_PART_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
