1bc212420f1c4b7754435f2d602ea80150f52db4
Zinedean Meaker Saaiman <ST10348753@vcconnect.edu.za>
2024-05-30 19:43:43 +0200

    Comment // Display the main menu options.

--- Program.cs
+++ Program.cs
@@ -32,6 +32,7 @@ namespace ST10348753_PROG6221_POE_PART_1
             while (true)
             {
                 Console.Clear();
+                // Display the main menu options
                 Console.WriteLine("Enter a command number:\n1. Add Recipe\n2. List Recipes\n3. Select Recipe\n4. Exit");
                 int command = ReadInt();
                 switch (command)
